fbcf3679e2c22c947302c7aa4c69a6fc67912f2d
ByteBard <liumingvic@gmail.com>
2022-09-13 18:26:40 +1000

    CRUD api added

--- CWRetail/CWRetail/Controllers/ProductController.cs
+++ CWRetail/CWRetail/Controllers/ProductController.cs
@@ -2,24 +2,94 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace CWRetail.Controllers
 {
     [Route("api/Product")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductController
+        : ControllerBase
     {
         private readonly DataContext _context;
-
+        private readonly string _invalidPriceMsg = "invalid price (should be 2 digit decimal)..";
         public ProductController(DataContext context)
         {
             _context = context;
         }
 
-        [HttpGet("GetAllProduct")]
-        public async Task<ActionResult<List<Product>>> Get()
+        [HttpGet("GetAllProduct/{sortBy?}")]
+        public async Task<ActionResult<List<Product>>> Get(string? sortBy = null)
         {
-            return Ok(await _context.Products.ToListAsync());
+            switch (sortBy?.ToLower())
+            {
+                case "type":
+                    return Ok(await _context.Products.OrderBy(x => x.Type).ToListAsync());
+                case "price":
+                    return Ok(await _context.Products.OrderBy(x => x.Price).ToListAsync());
+                default:
+                    return Ok(await _context.Products.OrderBy(x => x.Name).ToListAsync());
+            }
+            
+        }
+
+        [HttpPost("CreateProduct/{name}/{price}/{type}")]
+        public async Task<ActionResult> Create(string name, string price, string type)
+        {
+            var priceRegx = new Regex("[0-9]?[0-9]?(\\.[0-9][0-9]?)?");
+            var isValidPrice = priceRegx.IsMatch(price);
+            
+            if (!isValidPrice || !double.TryParse(price, out var priceNum)) return BadRequest(_invalidPriceMsg);
+
+            var product = new Product
+            {
+                Name = name,
+                Price = priceNum,
+                Type = type,
+                Active = true
+            };
+
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return Ok($"Id: {product.Id}, Name: {name} successfully created!");
+
+        }
+
+        [HttpPost("UpdateProduct/{id}/{name}/{price}/{type}")]
+        public async Task<ActionResult> Update(int id, string name, string price, string type)
+        {
+            var priceRegx = new Regex("[0-9]?[0-9]?(\\.[0-9][0-9]?)?");
+            var isValidPrice = priceRegx.IsMatch(price);
+
+            if (!isValidPrice || !double.TryParse(price, out var priceNum)) return BadRequest(_invalidPriceMsg);
+            var existingProduct = _context.Products.SingleOrDefault(b => b.Id == id);
+            if (existingProduct != null)
+            {
+                existingProduct.Name = name;
+                existingProduct.Type = type;
+                existingProduct.Price = priceNum;
+                await _context.SaveChangesAsync();
+                return Ok($"Id: {existingProduct.Id}, Name: {name} successfully updated!");
+            }
+
+            return BadRequest($"Id: {id}, Name: {name} updated failed (no such record)!");
+        }
+
+        [HttpPost("Delete/{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existingProduct = _context.Products.SingleOrDefault(b => b.Id == id);
+            if(existingProduct != null)
+            {
+                _context.Products.Remove(existingProduct);
+                await _context.SaveChangesAsync();
+                return Ok($"Id: {existingProduct.Id}, Name: {existingProduct} successfully deleted!");
+            }
+
+            return BadRequest($"Id: {id}, removed failed (no such record)!");
         }
     }
 }
